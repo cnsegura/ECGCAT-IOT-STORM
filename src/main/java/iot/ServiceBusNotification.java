c8393c04f396f267f5e7768fcbc814360e202889
chris <chris@Hawking>
2016-03-15 13:14:35 -0700

    fixed serialization error, now fighting null exception

--- src/main/java/iot/ServiceBusNotification.java
+++ src/main/java/iot/ServiceBusNotification.java
@@ -7,43 +7,43 @@ import storm.trident.tuple.TridentTuple;
 import com.microsoft.windowsazure.services.servicebus.*;
 import com.microsoft.windowsazure.services.servicebus.models.*;
 
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 
 import com.microsoft.windowsazure.Configuration;
 import com.microsoft.windowsazure.core.*;
+//import com.microsoft.windowsazure.exception.ServiceException;
 import com.microsoft.windowsazure.exception.ServiceException;
+
+import java.util.Map;
 
 import javax.xml.datatype.*;
 
 //This class sends a message to Azure Service Bus.
 public class ServiceBusNotification extends BaseFunction {
+	private Configuration config;
+	private ServiceBusContract service;
+	private TopicInfo topicInfo;
 	
-	//Configuration config = ServiceBusConfiguration.configureWithSASAuthentication("ecgcat-iot-servicebus", "RootManageSharedAccessKey", "WMOWbISHCy+EkplmooMGQSUuo0ONa/ALPXh6jhSWFto=", ".servicebus.windows.net");
-	//ServiceBusContract service = ServiceBusService.create(config);
-	//TopicInfo topicInfo = new TopicInfo("TempData");
+	public void prepare(Map conf, TopologyContext context) {
+		config = ServiceBusConfiguration.configureWithSASAuthentication("ecgcat-iot-servicebus", "RootManageSharedAccessKey", "INSERTKEYHERE", ".servicebus.windows.net");
+		service = ServiceBusService.create(config);
+		//TopicInfo topicInfo = new TopicInfo("tempdata");
+	}
 	
 	@Override
 	public void execute(TridentTuple tple, TridentCollector collector) {
 		//for debug
-		System.out.println("in service bus");
-						
-		/*
+		
+		BrokeredMessage message = new BrokeredMessage("Temperature");
 		try {
-			GetTopicResult getTopic = service.getTopic("TempData");
-			BrokeredMessage message = new BrokeredMessage("Temperature Out of Range");
-			service.sendTopicMessage("TempData", message);
+			System.out.println("in service bus");
+			service.sendTopicMessage("tempdata", message);
 		} catch (ServiceException e) {
-			try {
-				CreateTopicResult result = service.createTopic(topicInfo);
-				BrokeredMessage message = new BrokeredMessage("Temperature Out of Range");
-				service.sendTopicMessage("TempData", message);
-			} catch (ServiceException e1) {
-				System.out.print("Service Unavailable");
-				System.exit(-1);
-			}
-			
+			System.out.println("send data bombed");
+			//e.printStackTrace();
 		}
-		*/
+
 	}
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
     	// this bolt does not emit anything
